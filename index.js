df4aa433194590dbafd7bf89d07a12e8c7231868
Susan Minott <susan_minott@yahoo.com>
2022-05-02 21:11:54 -0400

    commit tree c89d97c6c29b0eee08731bc3be3388e90b4a1811

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+
+// 1st code
+//lines 5-6 also passed
+// const main = document.querySelector('main');
+// main.remove();
+document.querySelector('main').remove();
+
+//2nd Code
+
+const newHeader = document.createElement('h1');
+document.body.append(element)
+
+h1.className.add('victory')
+
+
+h1.innerHTML = 'YOUR-NAME is the champion'
+
+
